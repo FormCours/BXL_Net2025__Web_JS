07f20638ba8ee0d0ae18fe1f45ae5c4e8298e60f
Aude Beurive <aude.beurive@bstorm.be>
2025-10-30 14:30:58 +0100

    debut demo dom

---
+++ Demo_04_BasesDOM/main.js
@@ -0,0 +1,17 @@
+//! DOM
+//? Document Object Model
+document.title = 'Demo Bases Manipulations DOM';
+
+//! Récupérer des éléments HTML
+
+//! Modifier des éléments HTML
+//? Texte
+
+//? Attributs
+
+//? Valeur
+
+//! Ajouter des évènements
+
+
+//! Jouer avec le style
